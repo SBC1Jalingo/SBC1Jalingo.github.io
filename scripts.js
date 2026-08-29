47ce023c71cdd2dffd989a16a5f29a49e5a19380
olive44-star <okediran.olive@gmail.com>
2026-08-29 17:42:25 +0100

    adding buttons
    forward and backward button

--- scripts.js
+++ scripts.js
@@ -22,3 +22,78 @@ images.forEach(function(image) {
 closeButton.addEventListener("click", function() {
     modal.style.display = "none";
 });
+
+
+
+
+// Get the modal
+const modal = document.getElementById("imageModal");
+
+// Get the large image
+const largeImage = document.getElementById("largeImage");
+
+// Get the close button
+const closeButton = document.querySelector(".close");
+
+// Get the previous and next buttons
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
+
+// Get all gallery images
+const galleryImages = document.querySelectorAll(".gallery-image");
+
+// Keep track of which image we're viewing
+let currentImage = 0;
+
+
+// Open the popup when an image is clicked
+galleryImages.forEach((image, index) => {
+
+    image.addEventListener("click", function () {
+
+        currentImage = index;
+
+        largeImage.src = galleryImages[currentImage].src;
+
+        modal.style.display = "block";
+    });
+
+});
+
+
+// Show the previous image
+prevButton.addEventListener("click", function () {
+
+    currentImage--;
+
+    // If we're at the first image, go to the last image
+    if (currentImage < 0) {
+        currentImage = galleryImages.length - 1;
+    }
+
+    largeImage.src = galleryImages[currentImage].src;
+
+});
+
+
+// Show the next image
+nextButton.addEventListener("click", function () {
+
+    currentImage++;
+
+    // If we're at the last image, go back to the first image
+    if (currentImage >= galleryImages.length) {
+        currentImage = 0;
+    }
+
+    largeImage.src = galleryImages[currentImage].src;
+
+});
+
+
+// Close the popup
+closeButton.addEventListener("click", function () {
+
+    modal.style.display = "none";
+
+});
