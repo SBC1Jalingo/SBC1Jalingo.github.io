ae226b824688c860e9e13929f7fb52652bdab6a1
olive44-star <okediran.olive@gmail.com>
2026-09-02 19:02:10 +0100

    adding top andbutton button

--- scripts.js
+++ scripts.js
@@ -45,3 +45,43 @@ nextButton.addEventListener("click", function() {
 largeImage.src = images[currentImage].src;
 imageCaption.textContent = images[currentImage].alt;
 });
+
+
+
+
+
+
+
+
+
+
+// Back to Top button
+const backToTop = document.getElementById("backToTop");
+
+backToTop.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+// Go to Contact button
+const goToContact = document.getElementById("goToContact");
+
+goToContact.addEventListener("click", function() {
+    document.getElementById("Contact").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
